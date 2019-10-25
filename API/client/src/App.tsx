6a872df441426d72a97ea885b5fd436e2c31d03e
Lee Roz <anderl85@gmatc.matc.edu>
2019-10-24 22:41:56 -0500

    added react frontend

---
+++ API/client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Expense Monitor
+      </header>
+    </div>
+  );
+}
+
+export default App;
